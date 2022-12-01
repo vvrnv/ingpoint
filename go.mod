b43da5dc9a90fad5f82f9f61d842d882b58fc798
vvrnv <valeravoronov96@gmail.com>
2022-12-01 21:43:22 +0300

    add logs

--- go.mod
+++ go.mod
@@ -2,10 +2,14 @@ module github.com/vvrnv/ingpoint
 
 go 1.19
 
-require github.com/bitfield/script v0.21.3
+require (
+	github.com/bitfield/script v0.21.3
+	github.com/sirupsen/logrus v1.9.0
+)
 
 require (
 	bitbucket.org/creachadair/shell v0.0.7 // indirect
 	github.com/itchyny/gojq v0.12.7 // indirect
 	github.com/itchyny/timefmt-go v0.1.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- ingpoint.go
+++ ingpoint.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/bitfield/script"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: false,
+		FullTimestamp: true,
+	})
+
 	query := fmt.Sprintln(".items[].spec.rules[].host")
-	_, err := script.Exec("kubectl get -A ing -ojson").JQ(query).Freq().Column(2).Stdout()
+	ingresses, err := script.Exec("kubectl get ing -A -ojson").JQ(query).Freq().Column(2).Stdout()
 	if err != nil {
 		log.Fatal(err)
+	}
+	if ingresses == 0 {
+		log.Info("No resources found")
 	}
 }
